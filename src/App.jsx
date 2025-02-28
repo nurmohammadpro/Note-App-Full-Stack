76d0353fc664bc8dda0999d353601a5e92576b7f
Nur <nur_mohammad1@outlook.com>
2025-02-28 23:51:57 +0600

    Registration Feature is complete now!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe;
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute user={user}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
